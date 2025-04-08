851f1ff3c8a0945f32b5799630de3651005ac328
datdev <Dotiendat1751998@gmail.com>
2025-04-08 15:12:36 +0700

    update ICategoryService

--- src/main/java/com/datdev/service/ICategoryService.java
+++ src/main/java/com/datdev/service/ICategoryService.java
@@ -1,12 +1,13 @@
 package com.datdev.service;
 
 import com.datdev.model.CategoryModel;
-import com.datdev.model.NewsModel;
 
 import java.util.List;
 
 public interface ICategoryService {
     List<CategoryModel> findAll();
-    CategoryModel findOne (Long  id);
-    CategoryModel findOneByCategoryCode (String  categoryCode);
+
+    CategoryModel findOne(Long id);
+
+    CategoryModel findOneByCategoryCode(String categoryCode);
 }
