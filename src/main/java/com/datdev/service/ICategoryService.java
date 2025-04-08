b76a49115b8b6c8e911d599b5fe0a9cc91149b98
datdev <Dotiendat1751998@gmail.com>
2025-04-08 15:15:34 +0700

    update CategoryService

--- src/main/java/com/datdev/service/ICategoryService.java
+++ src/main/java/com/datdev/service/ICategoryService.java
@@ -9,5 +9,5 @@ public interface ICategoryService {
 
     CategoryModel findOne(Long id);
 
-    CategoryModel findOneByCategoryCode(Long categoryCode);
+    CategoryModel findOneByCategoryCode(String categoryCode);
 }
