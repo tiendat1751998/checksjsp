260c0441c3d14f841193fe488697ad741f8c7be5
datdev <Dotiendat1751998@gmail.com>
2025-04-08 15:09:47 +0700

    update SystemConstant

--- src/main/java/com/datdev/constant/SystemConstant.java
+++ src/main/java/com/datdev/constant/SystemConstant.java
@@ -1,6 +1,7 @@
 package com.datdev.constant;
 
 public class SystemConstant {
+//
     public  static final  String MODEL = "model";
     public  static final  String ADMIN = "ADMIN";
     public  static final  String USER=  "USER";
